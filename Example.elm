eea51d528a8dff2f70b9bf4ae903a84eae56d13f
John Kelly <jdrkelly14@gmail.com>
2018-01-21 20:58:33 -0800

    example 3

--- Example.elm
+++ Example.elm
@@ -1,6 +1,8 @@
 import PostgRest as PG
     exposing
         ( Attribute
+        , HasOne
+        , Relationship
         , Request
         , Schema
         , Selection
@@ -16,6 +18,7 @@ type alias Article =
     { title : String
     , body : String
     , favoritesCount : Int
+    , author : Author
     }
 
 
@@ -25,13 +28,34 @@ articleSelection :
             | title : Attribute String
             , body : Attribute String
             , favoritesCount : Attribute String
+            , author : Relationship HasOne UserSchema
         }
         Article
 articleSelection =
-    PG.map3 Article
+    PG.map4 Article
         (PG.field .title)
         (PG.field .body)
         (PG.field .favoritesCount)
+        (PG.embedOne .author userSchema authorSelection)
+
+
+type alias Author =
+    { name : String
+    , image : String
+    }
+
+
+authorSelection :
+    Selection
+        { attributes
+            | name : Attribute String
+            , image : Attribute String
+        }
+        Author
+authorSelection =
+    PG.map2 Author
+        (PG.field .name)
+        (PG.field .image)
 
 
 articleSchema :
@@ -39,10 +63,30 @@ articleSchema :
         { title : Attribute String
         , body : Attribute String
         , favoritesCount : Attribute Int
+        , author : Relationship HasOne UserSchema
         }
 articleSchema =
     PG.schema "articles"
         { title = PG.string "title"
         , body = PG.string "body"
         , favoritesCount = PG.int "favorites_count"
+        , author = PG.hasOne "author_id"
+        }
+
+
+type UserSchema
+    = UserSchema UserSchema
+
+
+userSchema :
+    Schema UserSchema
+        { name : Attribute String
+        , bio : Attribute String
+        , image : Attribute String
+        }
+userSchema =
+    PG.schema "users"
+        { name = PG.string "name"
+        , bio = PG.string "bio"
+        , image = PG.string "image"
         }
